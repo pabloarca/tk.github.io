6decdf922a8367a59179a8de8ac690f9b351b4bd
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-02-04 16:52:37 +0100

    Update form-process.php

--- php/form-process.php
+++ php/form-process.php
@@ -32,7 +32,7 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "pablo_mlg22@hotmail.com";
+$EmailTo = "pabloarcajaime@gmail.com";
 $Subject = "New Message Received";
 
 // prepare email body text
