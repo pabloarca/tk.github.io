4eec5796fe2fc46c70fb9585ebd6bb22fbab4d75
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-02-04 22:25:15 +0100

    Update send.php

--- send.php
+++ send.php
@@ -1,12 +1,20 @@
 <?php
+require 'PHPMailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$to = "info@terrakit.es"; // Nuestro correo de contacto
+// recogeremos los datos del formulario
+$nombre = $_POST['name'];
+$email = $_POST['email'];
 
-$to = "info@terrakit.es";
-
-$nombre = htmlentities($_POST['name']);
-$email = htmlentities($_POST['email']);
-
-if($nombre == "" || $email == "");
-  echo '<div class="alert alert-danger">Todos los campos son requeridos</div>';
+if($nombre == "" || $email == ""):
+	echo '<div class="alert alert-danger">Todos los campos son requeridos para el envio</div>';
 else:
+	$mail->From = $email;
+	$mail->addAddress($to);
+	
+	$mail->isHtml(true);
+	$mail->Body = '<strong>'.$nombre.'</strong> le ha contactado desde su web, y le ha enviado el siguiente mensaje: ;
+	$mail->CharSet = 'UTF-8';
+	$mail->send();
 endif;
 ?>
