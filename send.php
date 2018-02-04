784eed0e80e612d283ac5444342264be22e2e9cf
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-02-04 22:19:03 +0100

    Update send.php

--- send.php
+++ send.php
@@ -5,5 +5,8 @@ $to = "info@terrakit.es";
 $nombre = htmlentities($_POST['name']);
 $email = htmlentities($_POST['email']);
 
-echo $nombre.'<br> '.$email
+if($nombre == "" || $email == "");
+  echo '<div class="alert alert-danger">Todos los campos son requeridos</div>';
+else:
+endif;
 ?>
