4a649345974c39f9cf2f3dce33ad411ef4565b44
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-02-12 16:56:51 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -131,5 +131,23 @@
     "use strict";
     $('#loader').fadeOut();
    });
+ });
 
++/* CARRITO COMPRA
++==============================================================*/
+   
+$(‘.quantity’).on(‘click’, ‘.btn’, function(e) {
+e.preventDefault();
+var $this = $(this),
+$input = $this.closest(‘div’).find(‘input’),
+value = parseInt($input.val()),
+btn = $this.data(‘btn’);
 
+if (btn === ‘minus’) {
+value = value > 1 ? value – 1 : 0;
+} else {
+value = value < 100 ? value + 1 : 100;
+}
+
+$input.val(value);
+});   
