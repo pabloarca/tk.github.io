f9845d042a6818586368a47b7a5fdf2fe1d3c5f4
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-02-12 17:00:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -132,21 +132,3 @@
     $('#loader').fadeOut();
    });
 
-/* CARRITO COMPRA
-+==============================================================*/
-   
-$(‘.quantity’).on(‘click’, ‘.btn’, function(e) {
-e.preventDefault();
-var $this = $(this),
-$input = $this.closest(‘div’).find(‘input’),
-value = parseInt($input.val()),
-btn = $this.data(‘btn’);
-
-if (btn === ‘minus’) {
-value = value > 1 ? value – 1 : 0;
-} else {
-value = value < 100 ? value + 1 : 100;
-}
-
-$input.val(value);
-});   
