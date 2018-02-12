7ab06425ca59db36d2e592d17ac56135dbe3f258
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-02-12 16:34:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -131,3 +131,36 @@
     "use strict";
     $('#loader').fadeOut();
    });
+
+/* CARRITO COMPRA
+==============================================================*/
+$('.minus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value &amp;gt; 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+ 
+  $input.val(value);
+ 
+});
+ 
+$('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value &amp;lt; 100) {
+        value = value + 1;
+    } else {
+        value =145;
+    }
+ 
+    $input.val(value);
+});
