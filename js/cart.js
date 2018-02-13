40b43aa94ecbfa9e9c8af295a658a0e9152b45c8
pabloarca <31969557+pabloarca@users.noreply.github.com>
2018-02-13 17:57:22 +0100

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -58,7 +58,7 @@ jQuery(document).ready(function(){
     
 
     //fill total cost of our shopping cart 
-         document.getElementById("cart_total").innerHTML="4";
+         document.getElementById("cart_total").innerHTML=val(qty);
         	
 });    
 
